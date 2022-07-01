96d802badc2966206b2a37fa9a0ff1ef33deaf36
KaiYanagisawa <12730ky@gmail.com>
2022-07-01 09:20:57 +0900

    otoshidama_finish

--- beginnersSelection/otoshidama.cpp
+++ beginnersSelection/otoshidama.cpp
@@ -4,5 +4,24 @@ int main()
 {
     int N, Y;
     cin >> N >> Y;
+
+    int a{-1}, b{-1}, c{-1};
+
+    for (int i = 0; i <= N; i++)
+    {
+        for (int j = 0; j + i <= N; j++)
+        {
+            int k = N - i - j;
+            if (10000 * i + 5000 * j + 1000 * k == Y)
+            {
+                a = i;
+                b = j;
+                c = k;
+            }
+        }
+    }
+
+    cout << a << " " << b << " " << c << endl;
+
     return 0;
 }
