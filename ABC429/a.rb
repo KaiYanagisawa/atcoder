8ef5bac72bfde70e822be4a23bf3489ff737478e
KaiYanagisawa <12730ky@gmail.com>
2026-01-02 08:17:50 +0900

    Revert "measure"
    This reverts commit 6f439db2b1c15943be52a6fc64a2d3a1710ad765.

--- ABC429/a.rb
+++ ABC429/a.rb
@@ -3,30 +3,3 @@
 N, M = gets.split.map(&:to_i)
 
 N.times { puts _1 + 1 <= M ? 'OK' : 'Too Many Requests' }
-
-# require 'benchmark'
-
-# N, M = gets.split.map(&:to_i)
-
-# Benchmark.bm do |x|
-#   x.report { N.times { puts _1 + 1 <= M ? 'OK' : 'Too Many Requests' } }
-#   x.report { N.times { |a| puts a + 1 < M ? 'OK' : 'Too Many Requests' } }
-# end
-#
-# ===================================
-#
-# $ ruby "/Users/yanagisawakai/Documents/atcoder/ABC429/a.rb"
-# 5 3
-#        user     system      total        real
-#  OK
-# OK
-# OK
-# Too Many Requests
-# Too Many Requests
-#   0.000039   0.000069   0.000108 (  0.000090)
-#  OK
-# OK
-# Too Many Requests
-# Too Many Requests
-# Too Many Requests
-#   0.000012   0.000010   0.000022 (  0.000021)
